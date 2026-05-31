79372322e09cdcb87fecfc23bd514612b07eea28
Fabio Masunaga <fabiomasunaga26@gmail.com>
2026-05-31 08:14:18 -0300

    Corrige rotas publicas QR NFC fora do login

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,58 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-// LOGIN / ADMIN
+// páginas
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
-import AdminEdit from "./pages/AdminEdit";
-
-// ROTAS PÚBLICAS DAS TAGS
+import PessoaView from "./pages/PessoaView";
+import PetView from "./pages/PetView";
+import ResetPassword from "./pages/ResetPassword";
 import QrRedirect from "./pages/QrRedirect";
-import NfcView from "./pages/NfcView";
+import TagEntry from "./pages/TagEntry";
 import Escolha from "./pages/Escolha";
 
-import CadastroPet from "./pages/CadastroPet";
-import CadastroPessoa from "./pages/CadastroPessoa";
+// proteção
+import ProtectedRoute from "./components/ProtectedRoute";
 
-import PetView from "./pages/PetView";
-import PessoaView from "./pages/PessoaView";
-import TagEntry from "./pages/TagEntry";
-
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
+        {/* ROTAS PÚBLICAS QR / NFC */}
+        <Route path="/qr/:code" element={<QrRedirect />} />
+        <Route path="/nfc/:code" element={<QrRedirect />} />
+        <Route path="/ativar/:code" element={<TagEntry />} />
+        <Route path="/escolha/:code" element={<Escolha />} />
+
+        {/* VISUALIZAÇÃO PÚBLICA */}
+        <Route path="/pessoa/:code" element={<PessoaView />} />
+        <Route path="/pet/:code" element={<PetView />} />
+
+        {/* CADASTRO PÚBLICO */}
+        <Route path="/cadastro/pessoa/:code" element={<PessoaView />} />
+        <Route path="/cadastro/pet/:code" element={<PetView />} />
+
         {/* LOGIN */}
         <Route path="/login" element={<Login />} />
 
-        {/* ADMIN */}
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/admin/edit/:code" element={<AdminEdit />} />
+        {/* RESET DE SENHA */}
+        <Route path="/reset-password" element={<ResetPassword />} />
 
-        {/* ENTRADAS PÚBLICAS QR / NFC */}
-        <Route path="/qr/:code" element={<QrRedirect />} />
-        <Route path="/nfc/:code" element={<NfcView />} />
+        {/* ADMIN PROTEGIDO */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
 
-        {/* ESCOLHA / CADASTRO PÚBLICO */}
-        <Route path="/escolha/:code" element={<Escolha />} />
-        <Route path="/cadastro/pet/:code" element={<CadastroPet />} />
-        <Route path="/cadastro/pessoa/:code" element={<CadastroPessoa />} />
-
-        {/* VISUALIZAÇÃO PÚBLICA */}
-        <Route path="/pet/:code" element={<PetView />} />
-        <Route path="/pessoa/:code" element={<PessoaView />} />
-
-        {/* COMPATIBILIDADE COM LINKS ANTIGOS */}
-        <Route path="/view/pet/:code" element={<PetView />} />
-        <Route path="/view/pessoa/:code" element={<PessoaView />} />
-        <Route path="/perfil/pet/:code" element={<PetView />} />
-        <Route path="/perfil/pessoa/:code" element={<PessoaView />} />
-        <Route path="/perfil/:code" element={<NfcView />} />
-
-        {/* ENTRADAS GENÉRICAS ANTIGAS */}
-        <Route path="/tag/:code" element={<TagEntry />} />
-        <Route path="/ativar/:code" element={<TagEntry />} />
-
-        {/* RAIZ */}
+        {/* DEFAULT */}
         <Route path="/" element={<Navigate to="/login" replace />} />
-
-        {/* FALLBACK */}
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
+
+export default App;
